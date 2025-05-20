584aa9f0bb4d61504a66898be63275dabd95ebda
xirenaaa <falahauladi2006@mail.ugm.ac.id>
2025-05-21 00:34:47 +0700

    Add files via upload

---
+++ js calculator/modulPt10/pt10.js
@@ -0,0 +1,5 @@
+const eksternal = document.getElementById("eksternal");
+const tiga = document.getElementById("tiga");
+eksternal.addEventListener("click", function () {
+  tiga.innerText = "Tiga";
+});
